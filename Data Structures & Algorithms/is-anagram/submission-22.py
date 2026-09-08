c371950a4665fd89693f701fcdc7268d851f6a70
Roshan Ican <66337567+roshan-ican@users.noreply.github.com>
2026-09-08 19:42:55 +0400

    Add: is-anagram - submission-22

---
+++ Data Structures & Algorithms/is-anagram/submission-22.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = {}
+        t_count = {}
+
+        for c in s:
+            if c in s_count:
+                s_count[c] += 1
+            else:
+                s_count[c] = 1
+        
+        for c in t:
+            if c in t_count:
+                t_count[c] += 1
+            else:
+                t_count[c] = 1
+        
+        return s_count == t_count
+
+        
