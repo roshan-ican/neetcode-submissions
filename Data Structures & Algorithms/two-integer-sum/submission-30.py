a090cbd2e57a8e7af25bba6cdbbb8046d235aeda
Roshan Ican <66337567+roshan-ican@users.noreply.github.com>
2026-09-11 15:32:22 +0400

    Add: two-integer-sum - submission-30

---
+++ Data Structures & Algorithms/two-integer-sum/submission-30.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #     map = {}
+        #     for i in range(len(nums) - 1):
+        #         s = nums[i] + nums[i + 1]
+        #         if s == target:
+        #             return [i, i + 1]
+
+        #         complacent = target - nums[i]
+        #         if complacent in map:
+        #             return [map[complacent], i]
+        #         map[nums[i]] = i
+        #         class Solution:
+        # def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i in range(len(nums)):
+            complacent = target - nums[i]
+            if complacent in map:
+                return [map[complacent], i]
+            map[nums[i]] = i
